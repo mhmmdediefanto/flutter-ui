8dc0caf0411ca48f4e25881abc916c6c79df7ac6
mhmmdediefanto <ediefanto10@gmail.com>
2024-07-17 20:24:14 +0700

    membuat folder utilities untuk menangkap api laravel

---
+++ flutter_ui/lib/utilities/api_service.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/product.dart';
+
+Future<List<Product>> fetchProducts() async {
+  final response =
+      await http.get(Uri.parse('http://localhost:8000/api/product'));
+
+  if (response.statusCode == 200) {
+    List<dynamic> data = json.decode(response.body);
+    return data.map((json) => Product.fromJson(json)).toList();
+  } else {
+    throw Exception('Failed to load products');
+  }
+}
